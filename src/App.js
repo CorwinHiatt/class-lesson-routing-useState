a373ad40d18526844c101978e944a66cbe118577
CorwinHiatt <corwinhiatt@yahoo.com>
2023-02-07 13:56:08 -0500

    HOC

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+
 import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './scenes/home';
 import Welcome from './scenes/welcome';
 import './App.css';
